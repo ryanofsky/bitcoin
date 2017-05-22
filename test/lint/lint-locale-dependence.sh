f00110e8b6836ef384c0dbbd3ec693d5b97c21bb
Russell Yanofsky <russ@yanofsky.org>
2017-05-22 18:10:40 -0400

    Add fee_est tool for debugging fee estimation code

--- test/lint/lint-locale-dependence.sh
+++ test/lint/lint-locale-dependence.sh
@@ -11,6 +11,7 @@ KNOWN_VIOLATIONS=(
     "src/qt/rpcconsole.cpp:.*atoi"
     "src/rest.cpp:.*strtol"
     "src/test/dbwrapper_tests.cpp:.*snprintf"
+    "src/test/fee_est/fee_est.cpp:.*printf"
     "src/test/fuzz/parse_numbers.cpp:.*atoi"
     "src/torcontrol.cpp:.*atoi"
     "src/torcontrol.cpp:.*strtol"
