ae45a46043a3b8f9f0bc013ec20580b4801c2bf3
Russell Yanofsky <russ@yanofsky.org>
2017-05-22 18:09:19 -0400

    Add -estlog option for saving live fee estimation data

--- src/validation.cpp
+++ src/validation.cpp
@@ -23,6 +23,7 @@
 #include <node/ui_interface.h>
 #include <optional.h>
 #include <policy/fees.h>
+#include <policy/fees_input.h>
 #include <policy/policy.h>
 #include <policy/settings.h>
 #include <pow.h>
@@ -149,7 +150,8 @@ arith_uint256 nMinimumChainWork;
 CFeeRate minRelayTxFee = CFeeRate(DEFAULT_MIN_RELAY_TX_FEE);
 
 CBlockPolicyEstimator feeEstimator;
-CTxMemPool mempool(&feeEstimator);
+FeeEstInput feeEstimatorInput(feeEstimator);
+CTxMemPool mempool(&feeEstimatorInput);
 
 // Internal stuff
 namespace {
