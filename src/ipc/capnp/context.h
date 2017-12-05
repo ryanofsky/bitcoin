83673e73fd173f13803808b1e977b02b33d002aa
Russell Yanofsky <russ@yanofsky.org>
2017-12-05 15:57:12 -0500

    Add ipc::Context and ipc::capnp::Context structs
    These are currently empty structs but they will be used to pass some
    function and object pointers non-ipc code to ipc-code.

--- src/ipc/capnp/context.h
+++ src/ipc/capnp/context.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Copyright (c) 2021 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -21,3 +22,24 @@ struct Context : ipc::Context
 } // namespace ipc
 
 #endif // BITCOIN_IPC_CAPNP_CONTEXT_H
+||||||| merged common ancestors
+=======
+// Copyright (c) 2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_IPC_CAPNP_CONTEXT_H
+#define BITCOIN_IPC_CAPNP_CONTEXT_H
+
+#include <ipc/context.h>
+
+namespace ipc {
+namespace capnp {
+struct Context : ipc::Context
+{
+};
+} // namespace capnp
+} // namespace ipc
+
+#endif // BITCOIN_IPC_CAPNP_CONTEXT_H
+>>>>>>> Add ipc::Context and ipc::capnp::Context structs
--- src/ipc/context.h
+++ src/ipc/context.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Copyright (c) 2021 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -17,3 +18,21 @@ struct Context
 } // namespace ipc
 
 #endif // BITCOIN_IPC_CONTEXT_H
+||||||| merged common ancestors
+=======
+// Copyright (c) 2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_IPC_CONTEXT_H
+#define BITCOIN_IPC_CONTEXT_H
+
+namespace ipc {
+//! Context to give IPC protocol hooks access to application state.
+struct Context
+{
+};
+} // namespace ipc
+
+#endif // BITCOIN_IPC_CONTEXT_H
+>>>>>>> Add ipc::Context and ipc::capnp::Context structs
