49d784f4122229f1c406663d512910b4ea5eb01e
Russell Yanofsky <russ@yanofsky.org>
2017-12-05 15:57:12 -0500

    Get rid of Wallet::IsWalletFlagSet method
    Replace by privateKeysDisabled method to avoid need for GUI to reference
    internal wallet flags. Also remove adjaced WalletModel canGetAddresses wrapper
    that serves no purpose.

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -1254,7 +1254,7 @@ void BitcoinGUI::updateWalletStatus()
     }
     WalletModel * const walletModel = walletView->getWalletModel();
     setEncryptionStatus(walletModel->getEncryptionStatus());
-    setHDStatus(walletModel->privateKeysDisabled(), walletModel->wallet().hdEnabled());
+    setHDStatus(walletModel->wallet().privateKeysDisabled(), walletModel->wallet().hdEnabled());
 }
 #endif // ENABLE_WALLET
 
