44c7a5edc4a8d1fae1a4d29ee32426a26b5b56fe
Russell Yanofsky <russ@yanofsky.org>
2018-04-06 09:16:22 -0400

    |interface clang-format noise

--- src/interfaces/wallet.h
+++ src/interfaces/wallet.h
@@ -140,9 +140,7 @@ public:
         bilingual_str& fail_reason) = 0;
 
     //! Commit transaction.
-    virtual void commitTransaction(CTransactionRef tx,
-        WalletValueMap value_map,
-        WalletOrderForm order_form) = 0;
+    virtual void commitTransaction(CTransactionRef tx, WalletValueMap value_map, WalletOrderForm order_form) = 0;
 
     //! Return whether transaction can be abandoned.
     virtual bool transactionCanBeAbandoned(const uint256& txid) = 0;
