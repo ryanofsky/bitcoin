14849d8b245f47bb844e3173dc7ce209e8010ebf
Russell Yanofsky <russ@yanofsky.org>
2019-04-28 19:08:26 -0400

    Add <datadir>/settings.json persistent settings storage.
    Persistent settings are used in followup PRs #15936 to unify gui settings
    between bitcoin-qt and bitcoind, and #15937 to add a load_on_startup flag to
    the loadwallet RPC and maintain a dynamic list of wallets that should be loaded
    on startup that also can be shared between bitcoind and bitcoin-qt.

--- src/util/system.h
+++ src/util/system.h
@@ -41,6 +41,7 @@
 int64_t GetStartupTime();
 
 extern const char * const BITCOIN_CONF_FILENAME;
+extern const char * const BITCOIN_SETTINGS_FILENAME;
 
 void SetupEnvironment();
 bool SetupNetworking();
@@ -332,6 +333,41 @@ public:
      * Return nullopt for unknown arg.
      */
     Optional<unsigned int> GetArgFlags(const std::string& name) const;
+
+    /**
+     * Initialize read-write settings.
+     */
+    bool InitSettings(std::string& error);
+
+    /**
+     * Return whether read-write settings are enabled. This is true unless
+     * bitcoin was configured with -nosettings to disable storing changed
+     * dynamic settings from the GUI or RPC methods.
+     */
+    bool EnableSettingsFile();
+
+    /**
+     * Load <datadir>/settings.json file with saved settings. This needs to be
+     * called after SelectParams() because the settings file is network-specific.
+     * Errors are appended to vector if provided, otherwise logged.
+     */
+    bool ReadSettingsFile(std::vector<std::string>* errors = nullptr);
+
+    /**
+     * Save <datadir>/settings.json file with persistent settings.
+     * Errors are appended to vector if provided, otherwise logged.
+     */
+    bool WriteSettingsFile(std::vector<std::string>* errors = nullptr) const;
+
+    /**
+     * Access settings with lock held.
+     */
+    template <typename Fn>
+    void LockSettings(Fn&& fn)
+    {
+        LOCK(cs_args);
+        fn(m_settings);
+    }
 
     /**
      * Log the config file options and the command line arguments,
