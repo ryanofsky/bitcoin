ab60ca3c00fbb76bac6894e61765a55d958dad86
Russell Yanofsky <russ@yanofsky.org>
2019-04-29 15:29:00 -0400

    Unify bitcoin-qt and bitcoind persistent settings
    If a bitcoind setting like pruning, port mapping, or a network proxy is enabled
    in the GUI, it will now be stored in the bitcoin persistent setting file and
    shared with bitcoind, instead of being stored as Qt settings backed by the
    windows registry or platform specific config files.

--- src/qt/test/test_main.cpp
+++ src/qt/test/test_main.cpp
@@ -9,6 +9,7 @@
 #include <interfaces/node.h>
 #include <qt/bitcoin.h>
 #include <qt/test/apptests.h>
+#include <qt/test/optiontests.h>
 #include <qt/test/rpcnestedtests.h>
 #include <qt/test/uritests.h>
 #include <qt/test/compattests.h>
@@ -74,6 +75,10 @@ int main(int argc, char* argv[])
     node->context()->args->ClearArgs(); // Clear added args again
     AppTests app_tests(app);
     if (QTest::qExec(&app_tests) != 0) {
+        fInvalid = true;
+    }
+    OptionTests options_tests(*node);
+    if (QTest::qExec(&options_tests) != 0) {
         fInvalid = true;
     }
     URITests test1;
