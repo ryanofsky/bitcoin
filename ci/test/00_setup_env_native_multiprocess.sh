eb810bc9306cbd3531fd6061ee45e7072a8fc652
Russell Yanofsky <russ@yanofsky.org>
2019-07-09 14:27:39 -0400

    Add multiprocess travis configuration

--- ci/test/00_setup_env_native_multiprocess.sh
+++ ci/test/00_setup_env_native_multiprocess.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/usr/bin/env bash
 #
 # Copyright (c) 2020 The Bitcoin Core developers
@@ -12,3 +13,20 @@ export DEP_OPTS="MULTIPROCESS=1"
 export GOAL="install"
 export BITCOIN_CONFIG=""
 export TEST_RUNNER_ENV="BITCOIND=bitcoin-node"
+||||||| merged common ancestors
+=======
+#!/usr/bin/env bash
+#
+# Copyright (c) 2020 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+export LC_ALL=C.UTF-8
+
+export CONTAINER_NAME=ci_native_multiprocess
+export PACKAGES="cmake python3 libc6-dev-amd64-cross"
+export DEP_OPTS="MULTIPROCESS=1"
+export GOAL="install"
+export BITCOIN_CONFIG=""
+export TEST_RUNNER_ENV="BITCOIND=bitcoin-node"
+>>>>>>> Add multiprocess travis configuration
