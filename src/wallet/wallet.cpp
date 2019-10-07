da93efc30a782a20546e952e22d628480dda2c9a
Andrew Chow <achow101-github@achow101.com>
2019-10-07 14:11:34 -0400

    Key pool: Move TopUp call
    This avoids extra keyman map lookup:
    https://github.com/bitcoin/bitcoin/pull/16341#discussion_r331229987, but may be
    "annoying" and lead to unnecessary topups in some situations:
    https://github.com/bitcoin/bitcoin/pull/16341#issuecomment-510903836

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3058,8 +3058,6 @@ bool CWallet::GetNewChangeDestination(const OutputType type, CTxDestination& des
     LOCK(cs_wallet);
     error.clear();
 
-    m_spk_man->TopUp();
-
     ReserveDestination reservedest(this);
     if (!reservedest.GetReservedDestination(type, dest, true)) {
         error = "Error: Keypool ran out, please call keypoolrefill first";
@@ -3234,6 +3232,7 @@ bool ReserveDestination::GetReservedDestination(const OutputType type, CTxDestin
         return false;
     }
 
+    m_spk_man->TopUp();
 
     if (nIndex == -1)
     {
