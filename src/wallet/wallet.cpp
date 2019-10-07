f142acb176fa3c790514dd792544a11087c5007a
Andrew Chow <achow101-github@achow101.com>
2019-10-07 14:11:34 -0400

    Key pool: Change ReturnDestination interface to take address instead of key
    The address argument is currently unused, and not having the key argument
    requires the legacy keyman to maintain a new m_reserved_key_to_index map, so
    it's not clear what benefit of this change is, but it might help with hardware
    wallet support.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3240,11 +3240,9 @@ bool ReserveDestination::GetReservedDestination(const OutputType type, CTxDestin
         if (!m_spk_man->GetReservedDestination(type, internal, dest, nIndex, keypool)) {
             return false;
         }
-        vchPubKey = keypool.vchPubKey;
         fInternal = keypool.fInternal;
         address = dest;
     }
-    assert(vchPubKey.IsValid());
     return true;
 }
 
@@ -3253,17 +3251,15 @@ void ReserveDestination::KeepDestination()
     if (nIndex != -1)
         m_spk_man->KeepDestination(nIndex);
     nIndex = -1;
-    vchPubKey = CPubKey();
     address = CNoDestination();
 }
 
 void ReserveDestination::ReturnDestination()
 {
     if (nIndex != -1) {
-        m_spk_man->ReturnDestination(nIndex, fInternal, vchPubKey);
+        m_spk_man->ReturnDestination(nIndex, fInternal, address);
     }
     nIndex = -1;
-    vchPubKey = CPubKey();
     address = CNoDestination();
 }
 
