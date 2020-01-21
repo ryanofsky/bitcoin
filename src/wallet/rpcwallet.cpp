dcad66d87faddeb661fc794f2aa85b290b4112b6
Russell Yanofsky <russ@yanofsky.org>
2020-01-21 17:08:12 -0500

    wallet: Avoid use of Chain::Lock in rescanblockchain
    This is a step toward removing the Chain::Lock class and reducing cs_main
    locking.
    
    This change only affects behavior in the case where wallet last block processed
    falls behind the chain tip. The rescanblockchain error height error checking
    will just be stricter in this case and only accept values up to the last
    processed height

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -3576,7 +3576,13 @@ UniValue rescanblockchain(const JSONRPCRequest& request)
     }
 
     CWallet::ScanResult result =
+<<<<<<< HEAD
         pwallet->ScanForWalletTransactions(start_block, start_height, stop_height, reserver, true /* fUpdate */);
+||||||| merged common ancestors
+        pwallet->ScanForWalletTransactions(start_block, stop_block, reserver, true /* fUpdate */);
+=======
+        pwallet->ScanForWalletTransactions(start_block, stop_height, reserver, true /* fUpdate */);
+>>>>>>> wallet: Avoid use of Chain::Lock in rescanblockchain
     switch (result.status) {
     case CWallet::ScanResult::SUCCESS:
         break;
--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -887,7 +887,13 @@ public:
         //! USER_ABORT.
         uint256 last_failed_block;
     };
+<<<<<<< HEAD
     ScanResult ScanForWalletTransactions(const uint256& first_block, int block_height, Optional<int> max_height, const WalletRescanReserver& reserver, bool fUpdate);
+||||||| merged common ancestors
+    ScanResult ScanForWalletTransactions(const uint256& first_block, const uint256& last_block, const WalletRescanReserver& reserver, bool fUpdate);
+=======
+    ScanResult ScanForWalletTransactions(const uint256& first_block, Optional<int> max_height, const WalletRescanReserver& reserver, bool fUpdate);
+>>>>>>> wallet: Avoid use of Chain::Lock in rescanblockchain
     void TransactionRemovedFromMempool(const CTransactionRef &ptx) override;
     void ReacceptWalletTransactions() EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
     void ResendWalletTransactions();
