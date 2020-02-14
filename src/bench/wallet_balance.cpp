d375aa613ce7e9f13bc14f9c1f4d1f62c0337866
Russell Yanofsky <russ@yanofsky.org>
2020-02-14 12:07:00 -0500

    Drop Chain::requestMempoolTransactions method
    Benefits of the change:
    
    - Possibly faster wallet creation, avoids requesting mempool transactions when
      wallet is newly created.
    - Reduces scope of cs_main/cs_wallet/mempool locks. Gets rid of some recursive
      locking in postInitProcess.
    - Simplifies Chain interface by dropping requestMempoolTransactions method.
    - Consistently calls TransactionAddedToMempool without cs_main lock, instead of
      sometimes with the locks and sometimes without.
    - Stops sending transaction add/remove notifications out of order from multiple
      threads, and stops sending stale notifications from before notifications were
      requested. This simplifies Chain interface usage.
    - Improves performance over IPC. Mempool transactions are sent in a single IPC
      call instead of in a loop calling an IPC method repeatedly.

--- src/bench/wallet_balance.cpp
+++ src/bench/wallet_balance.cpp
@@ -32,7 +32,13 @@ static void WalletBalance(benchmark::State& state, const bool set_dirty, const b
         bool first_run;
         if (wallet.LoadWallet(first_run) != DBErrors::LOAD_OK) assert(false);
     }
+<<<<<<< HEAD
     auto handler = chain->handleNotifications({&wallet, [](CWallet*) {}});
+||||||| merged common ancestors
+    auto handler = chain->handleNotifications({ &wallet, [](CWallet*) {} });
+=======
+    CWallet::HandleNotifications({ &wallet, [](CWallet*) {} });
+>>>>>>> Drop Chain::requestMempoolTransactions method
 
     const Optional<std::string> address_mine{add_mine ? Optional<std::string>{getnewaddress(wallet)} : nullopt};
     if (add_watchonly) importaddress(wallet, ADDRESS_WATCHONLY);
