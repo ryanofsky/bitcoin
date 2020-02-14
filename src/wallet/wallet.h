8aa64ef7f5d6e02407aa09904092e0213dcdd2c4
Russell Yanofsky <russ@yanofsky.org>
2020-02-14 12:07:00 -0500

    refactor: Move cs_wallet to ReacceptWalletTransactions
    This is just a code simplification and revert of
    0440481c6bf5683eff669c789bdf6a306d99adc5 from
    https://github.com/bitcoin/bitcoin/pull/15652. No behavior is changing.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -949,7 +949,7 @@ public:
     };
     ScanResult ScanForWalletTransactions(const uint256& start_block, int start_height, std::optional<int> max_height, const WalletRescanReserver& reserver, bool fUpdate);
     void transactionRemovedFromMempool(const CTransactionRef& tx, MemPoolRemovalReason reason, uint64_t mempool_sequence) override;
-    void ReacceptWalletTransactions() EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
+    void ReacceptWalletTransactions() LOCKS_EXCLUDED(cs_wallet);
     void ResendWalletTransactions();
     struct Balance {
         CAmount m_mine_trusted{0};           //!< Trusted, at depth=GetBalance.min_depth or more
