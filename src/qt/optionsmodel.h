bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

ipc-connect # Add bitcoin-gui -ipcconnect option
Add bitcoin-gui -ipcconnect option

Add `-ipcconnect` option to `bitcoin-gui` to allow connecting gui to an
existing node instead of starting a new node process, so it is possible to
start and stop the gui independently of the node.

This change doesn't add an -ipcbind option to the bitcoin-wallet (which would
allow the gui to control wallet processes without going through the node), but
this would be a logical extension to add in the future.

src/qt/optionsmodel.h
@@ -9,6 +9,8 @@
 
 #include <QAbstractListModel>
 
+#include <assert.h>
+
 namespace interfaces {
 class Node;
 }
@@ -31,7 +33,7 @@ class OptionsModel : public QAbstractListModel
     Q_OBJECT
 
 public:
-    explicit OptionsModel(interfaces::Node& node, QObject *parent = nullptr, bool resetSettings = false);
+    explicit OptionsModel(QObject *parent, bool reset_settings);
 
     enum OptionID {
         StartAtStartup,         // bool
@@ -58,7 +60,7 @@ public:
         OptionIDRowCount,
     };
 
-    void Init(bool resetSettings = false);
+    void Init(bool resetSettings);
     void Reset();
 
     int rowCount(const QModelIndex & parent = QModelIndex()) const;
@@ -81,10 +83,11 @@ public:
     void setRestartRequired(bool fRequired);
     bool isRestartRequired() const;
 
-    interfaces::Node& node() const { return m_node; }
+    interfaces::Node& node() const { assert(m_nodelala != nullptr); return *m_nodelala; }
+    void setNode(interfaces::Node& node) { m_nodelala = &node; }
 
 private:
-    interfaces::Node& m_node;
+    interfaces::Node* m_nodelala = nullptr;
     /* Qt-only settings */
     bool fHideTrayIcon;
     bool fMinimizeToTray;

src/qt/test/addressbooktests.cpp
@@ -102,7 +102,7 @@ void TestAddAddressesToSendBook(interfaces::Node& node)
 
     // Initialize relevant QT models.
     std::unique_ptr<const PlatformStyle> platformStyle(PlatformStyle::instantiate("other"));
-    OptionsModel optionsModel(node);
+    OptionsModel optionsModel(nullptr /* parent */, false /* reset_settings */);
     ClientModel clientModel(node, &optionsModel);
     WalletModel walletModel(interfaces::MakeWallet(wallet), node, platformStyle.get(), &optionsModel, &clientModel);
     RemoveWallet(wallet);

src/qt/test/wallettests.cpp
@@ -160,7 +160,7 @@ void TestGUI(interfaces::Node& node)
     std::unique_ptr<const PlatformStyle> platformStyle(PlatformStyle::instantiate("other"));
     SendCoinsDialog sendCoinsDialog(platformStyle.get());
     TransactionView transactionView(platformStyle.get());
-    OptionsModel optionsModel(node);
+    OptionsModel optionsModel(nullptr /* parent */, false /* reset_settings */);
     ClientModel clientModel(node, &optionsModel);
     WalletModel walletModel(interfaces::MakeWallet(wallet), node, platformStyle.get(), &optionsModel, &clientModel);
     RemoveWallet(wallet);

ryanofsky/bitcoin

src/qt/optionsmodel.h
@@ -10,6 +10,8 @@
 
 #include <QAbstractListModel>
 
+#include <assert.h>
+
 namespace interfaces {
 class Node;
 }
@@ -38,7 +40,7 @@ class OptionsModel : public QAbstractListModel
     Q_OBJECT
 
 public:
-    explicit OptionsModel(interfaces::Node& node, QObject *parent = nullptr, bool resetSettings = false);
+    explicit OptionsModel(QObject *parent, bool resetSettings);
 
     enum OptionID {
         StartAtStartup,         // bool
@@ -65,7 +67,7 @@ public:
         OptionIDRowCount,
     };
 
-    void Init(bool resetSettings = false);
+    void Init(bool resetSettings);
     void Reset();
 
     int rowCount(const QModelIndex & parent = QModelIndex()) const;
@@ -91,10 +93,11 @@ public:
     void setRestartRequired(bool fRequired);
     bool isRestartRequired() const;
 
-    interfaces::Node& node() const { return m_node; }
+    interfaces::Node& node() const { assert(m_node != nullptr); return *m_node; }
+    void setNode(interfaces::Node& node) { m_node = &node; }
 
 private:
-    interfaces::Node& m_node;
+    interfaces::Node* m_node = nullptr;
     /* Qt-only settings */
     bool fHideTrayIcon;
     bool fMinimizeToTray;

src/qt/test/addressbooktests.cpp
@@ -106,7 +106,7 @@ void TestAddAddressesToSendBook(interfaces::Node& node)
 
     // Initialize relevant QT models.
     std::unique_ptr<const PlatformStyle> platformStyle(PlatformStyle::instantiate("other"));
-    OptionsModel optionsModel(node);
+    OptionsModel optionsModel(nullptr /* parent */, false /* resetSettings */);
     ClientModel clientModel(node, &optionsModel);
     AddWallet(wallet);
     WalletModel walletModel(interfaces::MakeWallet(wallet), clientModel, platformStyle.get());

src/qt/test/wallettests.cpp
@@ -168,7 +168,7 @@ void TestGUI(interfaces::Node& node)
     std::unique_ptr<const PlatformStyle> platformStyle(PlatformStyle::instantiate("other"));
     SendCoinsDialog sendCoinsDialog(platformStyle.get());
     TransactionView transactionView(platformStyle.get());
-    OptionsModel optionsModel(node);
+    OptionsModel optionsModel(nullptr /* parent */, false /* resetSettings */);
     ClientModel clientModel(node, &optionsModel);
     AddWallet(wallet);
     WalletModel walletModel(interfaces::MakeWallet(wallet), clientModel, platformStyle.get());