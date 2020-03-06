RandyMcMillan/dotfiles

ipc-build # libmultiprocess depends build
libmultiprocess depends build

depends/packages/native_boost.mk
@@ -1,0 +1,13 @@
+package=native_boost
+$(package)_version=$(boost_version)
+$(package)_download_path=$(boost_download_path)
+$(package)_file_name=$(boost_file_name)
+$(package)_sha256_hash=$(boost_sha256_hash)
+
+define $(package)_config_cmds
+  ./bootstrap.sh --prefix=$($($(package)_type)_prefix) --with-libraries=container
+endef
+
+define $(package)_stage_cmds
+  ./b2 -d0 --prefix=$($(package)_staging_prefix_dir) install
+endef

depends/packages/native_capnp.mk
@@ -1,0 +1,17 @@
+package=native_capnp
+$(package)_version=0.7.0
+$(package)_download_path=https://capnproto.org/
+$(package)_file_name=capnproto-c++-$($(package)_version).tar.gz
+$(package)_sha256_hash=c9a4c0bd88123064d483ab46ecee777f14d933359e23bff6fb4f4dbd28b4cd41
+
+define $(package)_config_cmds
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef

ryanofsky/bitcoin

depends/packages/native_boost.mk
@@ -1,0 +1,13 @@
+package=native_boost
+$(package)_version=$(boost_version)
+$(package)_download_path=$(boost_download_path)
+$(package)_file_name=$(boost_file_name)
+$(package)_sha256_hash=$(boost_sha256_hash)
+
+define $(package)_config_cmds
+  ./bootstrap.sh --prefix=$($($(package)_type)_prefix) --with-libraries=headers
+endef
+
+define $(package)_stage_cmds
+  ./b2 -d0 --prefix=$($(package)_staging_prefix_dir) install
+endef

depends/packages/native_capnp.mk
@@ -1,0 +1,18 @@
+package=native_capnp
+$(package)_version=0.7.0
+$(package)_download_path=https://capnproto.org/
+$(package)_download_file=capnproto-c++-$($(package)_version).tar.gz
+$(package)_file_name=capnproto-cxx-$($(package)_version).tar.gz
+$(package)_sha256_hash=c9a4c0bd88123064d483ab46ecee777f14d933359e23bff6fb4f4dbd28b4cd41
+
+define $(package)_config_cmds
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef