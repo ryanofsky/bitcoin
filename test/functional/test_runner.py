e208eaf2808b02152dac671902db6e80f28240c2
Ryan Ofsky <ryan@ofsky.org>
2020-05-14 10:33:05 -0400

    badbdb # test: add test for corrupt wallet bdb logs
    test: add test for corrupt wallet bdb logs

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -203,6 +203,7 @@ BASE_SCRIPTS = [
     'wallet_createwallet.py --legacy-wallet',
     'wallet_createwallet.py --usecli',
     'wallet_createwallet.py --descriptors',
+    'wallet_db.py',
     'wallet_watchonly.py --legacy-wallet',
     'wallet_watchonly.py --usecli --legacy-wallet',
     'wallet_reindex.py --legacy-wallet',
