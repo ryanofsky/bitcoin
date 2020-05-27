2ea00e3f804ddcaf20549207924f1560e4167977
Andrew Chow <achow101-github@achow101.com>
2020-05-26 20:53:48 -0400

    Implement SQLiteDatabase::Backup

--- src/wallet/sqlite.cpp
+++ src/wallet/sqlite.cpp
@@ -191,7 +191,28 @@ bool SQLiteDatabase::PeriodicFlush()
 
 bool SQLiteDatabase::Backup(const std::string& dest) const
 {
-    return false;
+    sqlite3* db_copy;
+    int res = sqlite3_open(dest.c_str(), &db_copy);
+    if (res != SQLITE_OK) {
+        sqlite3_close(db_copy);
+        return false;
+    }
+    sqlite3_backup* backup = sqlite3_backup_init(db_copy, "main", m_db, "main");
+    if (!backup) {
+        sqlite3_backup_finish(backup);
+        sqlite3_close(db_copy);
+        return false;
+    }
+    // Copy all of the pages using -1
+    res = sqlite3_backup_step(backup, -1);
+    if (res != SQLITE_DONE) {
+        sqlite3_backup_finish(backup);
+        sqlite3_close(db_copy);
+        return false;
+    }
+    res = sqlite3_backup_finish(backup);
+    sqlite3_close(db_copy);
+    return res == SQLITE_OK;
 }
 
 void SQLiteDatabase::Close()
