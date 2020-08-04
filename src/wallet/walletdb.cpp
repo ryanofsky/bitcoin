d3296bf105b4b2c6c5738a64742d8cb8296c0f9e
Russell Yanofsky <russ@yanofsky.org>
2020-08-04 19:33:37 -0400

    wallet: Remove Verify and IsLoaded methods
    Checks are now consolidated in MakeBerkeleyDatabase function instead of
    happening in higher level code.
    
    This commit does not change behavior except for error messages which now
    include more complete information.

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -1028,11 +1028,6 @@ std::unique_ptr<WalletDatabase> MakeDatabase(const fs::path& path, const Databas
     return MakeBerkeleyDatabase(path, options, status, error);
 }
 
-bool IsWalletLoaded(const fs::path& wallet_path)
-{
-    return IsBDBWalletLoaded(wallet_path);
-}
-
 /** Return object for accessing database at specified path. */
 std::unique_ptr<WalletDatabase> CreateWalletDatabase(const fs::path& path)
 {
