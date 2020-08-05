668568b453060e13a3cc402507fcbda21d3f6785
Russell Yanofsky <russ@yanofsky.org>
2020-08-04 21:03:27 -0400

    wallet: Remove path checking code from createwallet RPC
    This commit does not change behavior except for error messages which now
    include more complete information.

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2657,6 +2657,7 @@ static UniValue createwallet(const JSONRPCRequest& request)
 
     DatabaseOptions options;
     DatabaseStatus status;
+    options.require_create = true;
     options.create_flags = flags;
     options.create_passphrase = passphrase;
     bilingual_str error;
