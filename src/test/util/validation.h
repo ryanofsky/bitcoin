a18758360240fed3236ce57d60fed8c22905c581
Ryan Ofsky <ryan@ofsky.org>
2022-01-18 01:06:46 -0500

    indexes, refactor: Remove index RegisterValidationInterface call
    Add new interfaces::Chain::attachChain method and start moving index sync logic
    out of src/index/ to src/node/. In this commit, move code that registers for
    index validationinterface notifications. In later commits, move the actual sync
    thread and add real blockConnected, blockDisconnected, and chainStateFlushed
    implementations.
    
    This commit does not change behavior in any way.

--- src/test/util/validation.h
+++ src/test/util/validation.h
@@ -7,19 +7,21 @@
 
 #include <validation.h>
 
+<<<<<<< HEAD
 class CValidationInterface;
 
 struct TestChainstateManager : public ChainstateManager {
+||||||| parent of 20e68aefe8d6 (indexes, refactor: Remove index RegisterValidationInterface call)
+class CValidationInterface;
+
+struct TestChainState : public Chainstate {
+=======
+struct TestChainState : public Chainstate {
+>>>>>>> 20e68aefe8d6 (indexes, refactor: Remove index RegisterValidationInterface call)
     /** Reset the ibd cache to its initial state */
     void ResetIbd();
     /** Toggle IsInitialBlockDownload from true to false */
     void JumpOutOfIbd();
-};
-
-class ValidationInterfaceTest
-{
-public:
-    static void BlockConnected(CValidationInterface& obj, const std::shared_ptr<const CBlock>& block, const CBlockIndex* pindex);
 };
 
 #endif // BITCOIN_TEST_UTIL_VALIDATION_H
