1a938cc2115ef229d946b03913f7460fe1a81dab
Ryan Ofsky <ryan@ofsky.org>
2023-11-20 14:54:37 -0500

    test: add ipc test to test multiprocess type conversion code
    Add unit test to test IPC method calls and type conversion between bitcoin c++
    types and capnproto messages.
    
    Right now there are custom type hooks in bitcoin IPC code, so the test is
    simple, but in upcoming commits, code will be added to convert bitcoin types to
    capnproto messages, and the test will be expanded.

---
+++ src/test/ipc_test.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef IPC_TEST_H
+#define IPC_TEST_H
+
+#include <primitives/transaction.h>
+
+class FooImplementation
+{
+public:
+    int add(int a, int b) { return a + b; }
+};
+
+void IpcTest();
+
+#endif // IPC_TEST_H
