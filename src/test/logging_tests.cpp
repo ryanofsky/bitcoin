a71c30c23d0b6cd336288c854a3d9e4deae534be
Ryan Ofsky <ryan@ofsky.org>
2024-01-12 16:52:05 -0500

    log refactor: Allow log macros to accept context arguments
    Allow LogDebug(), LogTrace(), LogInfo(), LogWarning(), and LogError() macros to
    accept context arguments to provide more information in log messages and more
    control over logging to callers.
    
    This functionality is used in followup PRs:
    
    - https://github.com/bitcoin/bitcoin/pull/30342 - To let libbitcoinkernel send
      output to specfic `BCLog::Logger` instances instead of a global instance, so
      output can be disambiguated and applications can have more control over
      logging.
    
    - https://github.com/bitcoin/bitcoin/pull/30343 - To replace custom
      `WalletLogPrintf` calls with standard logging calls that automatically include
      wallet names and don't log everything at info level.
    
    This commit does not change behavior of current log prints or require them to
    be updated. It includes tests and documentation covering the new functionality.
    
    Co-Authored-By: Hodlinator <172445034+hodlinator@users.noreply.github.com>
    Co-Authored-By: stickies-v <stickies-v@protonmail.com>

--- src/test/logging_tests.cpp
+++ src/test/logging_tests.cpp
@@ -106,6 +106,7 @@ struct LogSetup : public BasicTestingSetup {
 };
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 //! Test calls to log macros with all possible types of arguments: with and
 //! without categories and with and without format arguments to make sure
 //! varargs are processed correctly.
@@ -177,6 +178,38 @@ BOOST_FIXTURE_TEST_CASE(logging_macro_args, LogSetup)
 
 ||||||| parent of f468c1b5402 (log test: Add test for currently accepted logging arguments)
 =======
+||||||| parent of 8517c14b77a (log refactor: Allow log macros to accept context arguments)
+=======
+//! Test logging to local logger.
+BOOST_AUTO_TEST_CASE(logging_local_logger)
+{
+    BCLog::Logger logger;
+    logger.m_log_timestamps = false;
+    logger.EnableCategory(BCLog::LogFlags::ALL);
+    logger.SetLogLevel(BCLog::Level::Trace);
+    BOOST_REQUIRE(logger.StartLogging());
+
+    std::vector<std::string> messages;
+    logger.PushBackCallback([&](const std::string& s) { messages.push_back(s); });
+
+    util::log::Context log{BCLog::NET, &logger};
+    LogError(log, "error %s", "arg");
+    LogWarning(log, "warning %s", "arg");
+    LogInfo(log, "info %s", "arg");
+    LogDebug(log, "debug %s", "arg");
+    LogTrace(log, "trace %s", "arg");
+
+    constexpr auto expected{std::to_array({
+        "[error] error arg\n",
+        "[warning] warning arg\n",
+        "info arg\n",
+        "[net] debug arg\n",
+        "[net:trace] trace arg\n",
+    })};
+    BOOST_CHECK_EQUAL_COLLECTIONS(messages.begin(), messages.end(), expected.begin(), expected.end());
+}
+
+>>>>>>> 8517c14b77a (log refactor: Allow log macros to accept context arguments)
 //! Test logging to global logger with different types of context arguments.
 BOOST_FIXTURE_TEST_CASE(logging_context_args, LogSetup)
 {
@@ -197,6 +230,19 @@ BOOST_FIXTURE_TEST_CASE(logging_context_args, LogSetup)
     LogDebug(BCLog::NET, "debug %s", "arg");
     LogTrace(BCLog::NET, "trace %s", "arg");
 
+    // Test logging with context object.
+    util::log::Context log{BCLog::TOR, &LogInstance()};
+    LogError(log, "error");
+    LogWarning(log, "warning");
+    LogInfo(log, "info");
+    LogDebug(log, "debug");
+    LogTrace(log, "trace");
+    LogError(log, "error %s", "arg");
+    LogWarning(log, "warning %s", "arg");
+    LogInfo(log, "info %s", "arg");
+    LogDebug(log, "debug %s", "arg");
+    LogTrace(log, "trace %s", "arg");
+
     const auto log_lines{ReadDebugLogLines()};
     constexpr auto expected{std::to_array({
         "[error] error",
@@ -212,6 +258,18 @@ BOOST_FIXTURE_TEST_CASE(logging_context_args, LogSetup)
 
         "[net] debug arg",
         "[net:trace] trace arg",
+
+        "[error] error",
+        "[warning] warning",
+        "info",
+        "[tor] debug",
+        "[tor:trace] trace",
+
+        "[error] error arg",
+        "[warning] warning arg",
+        "info arg",
+        "[tor] debug arg",
+        "[tor:trace] trace arg",
     })};
     BOOST_CHECK_EQUAL_COLLECTIONS(log_lines.begin(), log_lines.end(), expected.begin(), expected.end());
 }
--- src/util/log.h
+++ src/util/log.h
@@ -40,6 +40,9 @@ namespace util::log {
 /** Opaque to util::log; interpreted by consumers (e.g., BCLog::LogFlags). */
 using Category = uint64_t;
 
+/** Base class inherited by log consumers. Opaque like category, used for basic type-checking. */
+class Logger{};
+
 enum class Level {
     Trace = 0, // High-volume or detailed logging for development/debugging
     Debug,     // Reasonably noisy logging, but still usable in production
@@ -74,9 +77,10 @@ struct Entry {
 
 /** Return whether messages with specified category and level should be logged. Applications using
  * the logging library need to provide this. */
-bool ShouldLog(Category category, Level level);
+bool ShouldLog(Logger* logger, Category category, Level level);
 
 /** Send message to be logged. Applications using the logging library need to provide this. */
+<<<<<<< HEAD
 void Log(Entry entry);
 
 //! Object representing context of log messages. Holds a logging category and
@@ -148,8 +152,63 @@ void Log(Options options, SourceLocation&& source_loc, Context&& context, Contex
     Log(std::move(options), std::move(source_loc), context, fmt, args...);
 }
 } // namespace detail
+||||||| parent of 8517c14b77a (log refactor: Allow log macros to accept context arguments)
+void Log(Entry entry);
+=======
+void Log(Logger* logger, Entry entry);
+
+//! Object representing a context of log messages. Holds a logging category, an
+//! optional log pointer which can be used by the application's log handler to
+//! determine where to log to, and a Format hook to control message formatting.
+struct Context {
+    Category category;
+    Logger* logger;
+
+    explicit Context(Category category = BCLog::LogFlags::ALL, Logger* logger = nullptr) : category{category}, logger{logger} {}
+
+    template <typename... Args>
+    std::string Format(util::ConstevalFormatString<sizeof...(Args)> fmt, const Args&... args) const
+    {
+        std::string log_msg;
+        try {
+            log_msg = tfm::format(fmt, args...);
+        } catch (tinyformat::format_error& fmterr) {
+            log_msg = "Error \"" + std::string{fmterr.what()} + "\" while formatting log message: " + fmt.fmt;
+        }
+        return log_msg;
+    }
+};
+
+namespace detail {
+//! Internal helper to get log context object from the first macro argument.
+inline Context& GetContext(Context& context LIFETIMEBOUND) { return context; }
+inline Context GetContext(Category category) { return Context{category}; }
+inline Context GetContext(std::string_view fmt) { return Context{}; }
+
+//! Internal helper to format log arguments and call a logging function.
+//! Overloaded to detect case where first macro argument is a string literal and
+//! context has been omitted.
+template <typename Context, typename... Args>
+void Log(Level level, bool should_ratelimit, SourceLocation&& source_loc, Context&& context, ConstevalFormatString<sizeof...(Args)> fmt, const Args&... args)
+{
+    Log(context.logger, Entry{
+        .category = context.category,
+        .level = level,
+        .should_ratelimit = should_ratelimit,
+        .source_loc = std::move(source_loc),
+        .message = context.Format(fmt, args...)});
+}
+template <typename Context, typename ContextArg, typename... Args>
+requires (!std::is_convertible_v<ContextArg, std::string_view>)
+void Log(Level level, bool should_ratelimit, SourceLocation&& source_loc, Context&& context, ContextArg&&, ConstevalFormatString<sizeof...(Args)> fmt, const Args&... args)
+{
+    Log(level, should_ratelimit, std::move(source_loc), context, fmt, args...);
+}
+} // namespace detail
+>>>>>>> 8517c14b77a (log refactor: Allow log macros to accept context arguments)
 } // namespace util::log
 
+<<<<<<< HEAD
 //! Return the first argument from a variadic macro argument list.
 #define FIRST_ARG(arg, ...) arg
 
@@ -181,6 +240,43 @@ void Log(Options options, SourceLocation&& source_loc, Context&& context, Contex
 #define LogDebug(...) LogPrint((.level = util::log::Level::Debug), __VA_ARGS__)
 #define LogTrace(...) LogPrint((.level = util::log::Level::Trace), __VA_ARGS__)
 
+||||||| parent of 8517c14b77a (log refactor: Allow log macros to accept context arguments)
+=======
+//! Internal helper to return first arg in a __VA_ARGS__ pack.
+#define FirstArg_(arg, ...) arg
+
+//! Internal helper to conditionally log. Only evaluates arguments when needed.
+// Allow __func__ to be used in any context without warnings:
+// NOLINTBEGIN(bugprone-lambda-function-name)
+#define LogPrint_(level, should_ratelimit, ...)                                                    \
+    do {                                                                                           \
+        auto&& _context{util::log::detail::GetContext(FirstArg_(__VA_ARGS__))};                    \
+        if (util::log::ShouldLog(_context.logger, _context.category, (level))) {                   \
+            util::log::detail::Log((level), (should_ratelimit), SourceLocation{__func__},          \
+                                   _context, __VA_ARGS__);                                         \
+        } else if ((level) >= util::log::Level::Info) {                                            \
+            /* For Info levels and up, we guarantee that arguments are always evaluated. */        \
+            [](auto&&...) {}(__VA_ARGS__);                                                         \
+        }                                                                                          \
+    } while (0)
+// NOLINTEND(bugprone-lambda-function-name)
+
+//! Logging macros which output log messages at the specified levels. The
+//! macros accept an optional log context or category parameter followed by a
+//! printf-style format string and arguments.
+//!
+//! If severity level is Info or higher, rate limiting is applied to mitigate
+//! disk filling attacks. Users enabling logging at Debug and lower levels are
+//! assumed to be developers or power users who are aware that -debug may cause
+//! excessive disk usage due to logging.
+#define LogError(...) LogPrint_(util::log::Level::Error, /*should_ratelimit=*/true, __VA_ARGS__)
+#define LogWarning(...) LogPrint_(util::log::Level::Warning, /*should_ratelimit=*/true, __VA_ARGS__)
+#define LogInfo(...) LogPrint_(util::log::Level::Info, /*should_ratelimit=*/true, __VA_ARGS__)
+#define LogDebug(...) LogPrint_(util::log::Level::Debug, /*should_ratelimit=*/false, __VA_ARGS__)
+#define LogTrace(...) LogPrint_(util::log::Level::Trace, /*should_ratelimit=*/false, __VA_ARGS__)
+#define LogPrintLevel_(context, level, should_ratelimit, ...) LogPrint_((level), (should_ratelimit), (context), __VA_ARGS__)
+
+>>>>>>> 8517c14b77a (log refactor: Allow log macros to accept context arguments)
 namespace BCLog {
 //! Alias for compatibility. Prefer util::log::Level over BCLog::Level in new code.
 using Level = util::log::Level;
