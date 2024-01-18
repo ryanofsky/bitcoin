5dfd24581a7c5497601966a5371d8c33eabcee8a
Ryan Ofsky <ryan@ofsky.org>
2024-01-18 15:10:28 -0500

    depends: Update libmultiprocess library to fix C++20 macos build error
    Fixes #29248
    
    The std::result_of type was removed in c++20, but was being referenced in some
    old, unused code in the library. The issue was fixed in:
    
    https://github.com/chaincodelabs/libmultiprocess/pull/91 util: Drop Bind, BindTuple, ComposeFn, GetFn, and ThrowFn helpers
    
    This update also includes other recent libmultiprocess changes to improve C++20
    support and fix build issues:
    
    https://github.com/chaincodelabs/libmultiprocess/pull/89 pkgconfig: Drop -std=c++17 compile flag
    https://github.com/chaincodelabs/libmultiprocess/pull/90 pkgconfig: Use @CMAKE_INSTALL_LIBDIR@ variable

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=414542f81e0997354b45b8ade13ca144a3e35ff1
+$(package)_version=7d1fee06d65cefd4a59c51f1f6ea4754d0efbd99
 $(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=8542dbaf8c4fce8fd7af6929f5dc9b34dffa51c43e9ee360e93ee0f34b180bc2
+$(package)_sha256_hash=25db77d2ce7d668b75c5bc142ea22ec981d57224b563aec6e0f309684f9cfbb4
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds
