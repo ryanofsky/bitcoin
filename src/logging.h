2f2ff16f346dd67927eb78bffd00e112be118950
Ryan Ofsky <ryan@ofsky.org>
2024-02-27 20:45:00 -0500

    log, refactor: Add preprocessor workaround for MSVC
    Needed to fix errors like:
    
    const Context &GetContext(const Context &)': expects 1 arguments - 3 provided
    
    due to a compiler bug:
    
    https://stackoverflow.com/questions/5134523/msvc-doesnt-expand-va-args-correctly/5134656#5134656
    
    Example CI failure:
    
    https://github.com/bitcoin/bitcoin/actions/runs/8072891468/job/22055528830?pr=29256

--- src/logging.h
+++ src/logging.h
@@ -462,6 +462,7 @@ static inline bool LogAcceptCategory(BCLog::LogFlags category, BCLog::Level leve
 bool GetLogCategory(BCLog::LogFlags& flag, std::string_view str);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 ||||||| parent of 05d22deffdb (log, refactor: Allow log macros to accept context arguments)
 template <typename... Args>
 inline void LogPrintFormatInternal(SourceLocation&& source_loc, BCLog::LogFlags flag, BCLog::Level level, bool should_ratelimit, util::ConstevalFormatString<sizeof...(Args)> fmt, const Args&... args)
@@ -511,13 +512,23 @@ inline void LogPrintFormatInternal(SourceLocation&& source_loc, BCLog::LogFlags 
 =======
 //! Internal helper to return first arg in a __VA_ARGS__ pack.
 #define FirstArg_(arg, ...) arg
+||||||| parent of b9842125715 (log, refactor: Add preprocessor workaround for MSVC)
+//! Internal helper to return first arg in a __VA_ARGS__ pack.
+#define FirstArg_(arg, ...) arg
+=======
+//! Internal helper to return first arg in a __VA_ARGS__ pack. This could be
+//! simplified to `#define FirstArg_(arg, ...) arg` if not for a preprocessor
+//! bug in Visual C++.
+#define FirstArg_Impl(arg, ...) arg
+#define FirstArg_(args) FirstArg_Impl args
+>>>>>>> b9842125715 (log, refactor: Add preprocessor workaround for MSVC)
 
 //! Internal helper to conditionally log. Only evaluates arguments when needed.
 // Allow __func__ to be used in any context without warnings:
 // NOLINTBEGIN(bugprone-lambda-function-name)
 #define LogPrint_(level, ratelimit, ...)                                       \
     do {                                                                       \
-        const auto& ctx{BCLog::detail::GetContext(FirstArg_(__VA_ARGS__))};    \
+        const auto& ctx{BCLog::detail::GetContext(FirstArg_((__VA_ARGS__)))};  \
         if (const BCLog::Level lvl{level}; LogEnabled(ctx, lvl)) {             \
             const BCLog::LogFlags cat{ctx.category};                           \
             const bool rl{ratelimit};                                          \
