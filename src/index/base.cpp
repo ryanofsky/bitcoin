5e6b02060f20d8f6ae96d795a8d55d333b298f22
Ryan Ofsky <ryan@ofsky.org>
2024-03-15 13:52:11 -0400

    Remove direct index -> node dependency
    Replace last m_blockman.UpdatePruneLock call with
    interfaces::Chain::updatePruneLock call.

--- src/index/base.cpp
+++ src/index/base.cpp
@@ -303,11 +303,6 @@ bool BaseIndex::Init()
         assert(!m_notifications);
     }
 
-    // m_chainstate member gives indexing code access to node internals. It is
-    // removed in followup https://github.com/bitcoin/bitcoin/pull/24230
-    m_chainstate = WITH_LOCK(::cs_main,
-                             return &m_chain->context()->chainman->ValidatedChainstate());
-
     const auto locator{GetDB().ReadBestBlock()};
     std::optional<BlockRef> block;
     if (!locator.IsNull()) {
@@ -508,12 +503,12 @@ std::optional<interfaces::BlockRef> BaseIndex::GetBestBlock() const
 
 void BaseIndex::SetBestBlock(const std::optional<interfaces::BlockRef>& block)
 {
-    assert(!m_chainstate->m_blockman.IsPruneMode() || AllowPrune());
+    assert(!m_chain->pruningEnabled() || AllowPrune());
 
     if (AllowPrune() && block) {
         node::PruneLockInfo prune_lock;
         prune_lock.height_first = block->height;
-        WITH_LOCK(::cs_main, m_chainstate->m_blockman.UpdatePruneLock(GetName(), prune_lock));
+        m_chain->updatePruneLock(GetName(), prune_lock);
     }
 
     // Intentionally set m_best_block as the last step in this function,
