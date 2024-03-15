57447d7f6e500a6c98ca06d626ca4f8537f8f0e7
Ryan Ofsky <ryan@ofsky.org>
2024-03-15 13:52:11 -0400

    Remove direct index -> node dependency
    Replace last m_blockman.UpdatePruneLock call with
    interfaces::Chain::updatePruneLock call.

--- src/index/txindex.cpp
+++ src/index/txindex.cpp
@@ -19,7 +19,6 @@
 #include <uint256.h>
 #include <util/fs.h>
 #include <util/log.h>
-#include <validation.h>
 
 #include <cassert>
 #include <cstdint>
@@ -66,8 +65,16 @@ void TxIndex::DB::WriteTxs(const std::vector<std::pair<Txid, CDiskTxPos>>& v_pos
     WriteBatch(batch);
 }
 
+<<<<<<< HEAD
 TxIndex::TxIndex(std::unique_ptr<interfaces::Chain> chain, size_t n_cache_size, bool f_memory, bool f_wipe)
     : BaseIndex(std::move(chain), "txindex", "txidx"), m_db(std::make_unique<TxIndex::DB>(n_cache_size, f_memory, f_wipe))
+||||||| parent of 6e985cc72ad (Remove direct index -> node dependency)
+TxIndex::TxIndex(std::unique_ptr<interfaces::Chain> chain, size_t n_cache_size, bool f_memory, bool f_wipe)
+    : BaseIndex(std::move(chain), "txindex"), m_db(std::make_unique<TxIndex::DB>(n_cache_size, f_memory, f_wipe))
+=======
+TxIndex::TxIndex(std::unique_ptr<interfaces::Chain> chain, node::BlockManager& blockman, size_t n_cache_size, bool f_memory, bool f_wipe)
+    : BaseIndex(std::move(chain), "txindex"), m_db(std::make_unique<TxIndex::DB>(n_cache_size, f_memory, f_wipe)), m_blockman(blockman)
+>>>>>>> 6e985cc72ad (Remove direct index -> node dependency)
 {}
 
 TxIndex::~TxIndex() = default;
@@ -98,7 +105,7 @@ bool TxIndex::FindTx(const Txid& tx_hash, uint256& block_hash, CTransactionRef& 
         return false;
     }
 
-    AutoFile file{m_chainstate->m_blockman.OpenBlockFile(postx, true)};
+    AutoFile file{m_blockman.OpenBlockFile(postx, true)};
     if (file.IsNull()) {
         LogError("OpenBlockFile failed");
         return false;
