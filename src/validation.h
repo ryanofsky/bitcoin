b7ac4754cedb329c9e8ff59bc5b2c18110a8942b
Ryan Ofsky <ryan@ofsky.org>
2024-03-20 17:12:09 -0400

    refactor, validation: Return fatal errors from activate best chain functions
    Return fatal errors from ActivateBestChain, ActivateBestChainStep, and
    PreciousBlock functions.  Also add nodiscard annotations so callers handle the
    result values. Two callers, ProcessNewBlock and LoadExternalBlockFile, will be
    updated in an upcoming commits to bubble results up to its callers.

--- src/validation.h
+++ src/validation.h
@@ -714,7 +714,7 @@ public:
      *
      * @returns true unless a system error occurred
      */
-    bool ActivateBestChain(
+    [[nodiscard]] kernel::FlushResult<> ActivateBestChain(
         BlockValidationState& state,
         std::shared_ptr<const CBlock> pblock = nullptr)
         EXCLUSIVE_LOCKS_REQUIRED(!m_chainstate_mutex)
@@ -734,7 +734,7 @@ public:
      *
      * May not be called in a validationinterface callback.
      */
-    bool PreciousBlock(BlockValidationState& state, CBlockIndex* pindex)
+    [[nodiscard]] kernel::FlushResult<> PreciousBlock(BlockValidationState& state, CBlockIndex* pindex)
         EXCLUSIVE_LOCKS_REQUIRED(!m_chainstate_mutex)
         LOCKS_EXCLUDED(::cs_main);
 
@@ -786,8 +786,16 @@ public:
         return m_mempool ? &m_mempool->cs : nullptr;
     }
 
+<<<<<<< HEAD
 protected:
     bool ActivateBestChainStep(BlockValidationState& state, CBlockIndex* pindexMostWork, const std::shared_ptr<const CBlock>& pblock, bool& fInvalidFound, ConnectTrace& connectTrace) EXCLUSIVE_LOCKS_REQUIRED(cs_main, m_mempool->cs);
+||||||| parent of 38ad174dfbe5 (refactor, validation: Return fatal errors from activate best chain functions)
+private:
+    bool ActivateBestChainStep(BlockValidationState& state, CBlockIndex* pindexMostWork, const std::shared_ptr<const CBlock>& pblock, bool& fInvalidFound, ConnectTrace& connectTrace) EXCLUSIVE_LOCKS_REQUIRED(cs_main, m_mempool->cs);
+=======
+private:
+    [[nodiscard]] kernel::FlushResult<void, kernel::AbortFailure> ActivateBestChainStep(BlockValidationState& state, CBlockIndex* pindexMostWork, const std::shared_ptr<const CBlock>& pblock, bool& fInvalidFound, ConnectTrace& connectTrace) EXCLUSIVE_LOCKS_REQUIRED(cs_main, m_mempool->cs);
+>>>>>>> 38ad174dfbe5 (refactor, validation: Return fatal errors from activate best chain functions)
     [[nodiscard]] kernel::FlushResult<void, kernel::AbortFailure> ConnectTip(BlockValidationState& state, CBlockIndex* pindexNew, const std::shared_ptr<const CBlock>& pblock, ConnectTrace& connectTrace, DisconnectedBlockTransactions& disconnectpool) EXCLUSIVE_LOCKS_REQUIRED(cs_main, m_mempool->cs);
 
     void InvalidBlockFound(CBlockIndex* pindex, const BlockValidationState& state) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
