dab0e453435b0a495d4fcb589ecf9674018e87ab
Ryan Ofsky <ryan@ofsky.org>
2024-03-20 17:12:09 -0400

    refactor, validation: Return fatal errors from new block functions
    Return fatal errors from AcceptBlock, ProcessNewBlock, TestBlockValidity,
    LoadGenesisBlock, and LoadExternalBlockFile. Also add nodiscard annotations so
    callers handle the result values.

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -1262,7 +1262,8 @@ void ImportBlocks(ChainstateManager& chainman, std::span<const fs::path> import_
                 break; // This error is logged in OpenBlockFile
             }
             LogPrintf("Reindexing block file blk%05u.dat...\n", (unsigned int)nFile);
-            chainman.LoadExternalBlockFile(file, &pos, &blocks_with_unknown_parent);
+            // Ignore failure value, do not treat flush error as failure.
+            chainman.LoadExternalBlockFile(file, &pos, &blocks_with_unknown_parent) >> result;
             if (chainman.m_interrupt) {
                 LogPrintf("Interrupt requested. Exit %s\n", __func__);
                 return;
@@ -1273,7 +1274,8 @@ void ImportBlocks(ChainstateManager& chainman, std::span<const fs::path> import_
         chainman.m_blockman.m_blockfiles_indexed = true;
         LogPrintf("Reindexing finished\n");
         // To avoid ending up in a situation without genesis block, re-try initializing (no-op if reindexing worked):
-        chainman.ActiveChainstate().LoadGenesisBlock();
+        // Ignore failure value, do not treat flush error as failure.
+        chainman.ActiveChainstate().LoadGenesisBlock() >> result;
     }
 
     // -loadblock=
@@ -1281,7 +1283,8 @@ void ImportBlocks(ChainstateManager& chainman, std::span<const fs::path> import_
         AutoFile file{fsbridge::fopen(path, "rb")};
         if (!file.IsNull()) {
             LogPrintf("Importing blocks file %s...\n", fs::PathToString(path));
-            chainman.LoadExternalBlockFile(file);
+            // Ignore failure value, do not treat flush error as failure.
+            chainman.LoadExternalBlockFile(file) >> result;
             if (chainman.m_interrupt) {
                 LogPrintf("Interrupt requested. Exit %s\n", __func__);
                 return;
