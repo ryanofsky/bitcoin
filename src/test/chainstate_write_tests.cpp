8a1e60a723574d02258a12a9d31123b0d6f6c7be
Ryan Ofsky <ryan@ofsky.org>
2024-03-20 17:12:09 -0400

    refactor, validation: Return fatal errors from FlushStateToDisk
    Return fatal errors from the Chainstate::FlushStateToDisk method and several
    small, related methods which wrap it: ForceFlushStateToDisk, PruneAndFlush,
    ResizeCoinsCaches, and MaybeRebalanceCaches.
    
    Also add nodiscard annotations so callers do not accidentally ignore the result
    values. Callers in init and rpc files are updated to explicitly ignore the
    flush results, and other callers (AcceptToMemoryPool, ProcessNewPackage,
    DisconnectTip, ConnectTip, ActivateBestChainStep, ActivateSnapshot,
    MaybeCompleteSnapshotValidation) are updated to store the results in this
    commit, and will be updated in upcoming commits to bubble results up to their
    callers.

--- src/test/chainstate_write_tests.cpp
+++ src/test/chainstate_write_tests.cpp
@@ -30,18 +30,34 @@ BOOST_FIXTURE_TEST_CASE(chainstate_write_interval, TestingSetup)
     BlockValidationState state_dummy{};
 
     // The first periodic flush sets m_next_write and does not flush
-    chainstate.FlushStateToDisk(state_dummy, FlushStateMode::PERIODIC);
+    BOOST_CHECK(chainstate.FlushStateToDisk(state_dummy, FlushStateMode::PERIODIC));
     m_node.validation_signals->SyncWithValidationInterfaceQueue();
     BOOST_CHECK(!sub->m_did_flush);
 
     // The periodic flush interval is between 50 and 70 minutes (inclusive)
+<<<<<<< HEAD
     SetMockTime(GetTime<std::chrono::minutes>() + DATABASE_WRITE_INTERVAL_MIN - 1min);
     chainstate.FlushStateToDisk(state_dummy, FlushStateMode::PERIODIC);
+||||||| parent of ac6278f88983 (refactor, validation: Return fatal errors from FlushStateToDisk)
+    SetMockTime(GetTime<std::chrono::minutes>() + 49min);
+    chainstate.FlushStateToDisk(state_dummy, FlushStateMode::PERIODIC);
+=======
+    SetMockTime(GetTime<std::chrono::minutes>() + 49min);
+    BOOST_CHECK(chainstate.FlushStateToDisk(state_dummy, FlushStateMode::PERIODIC));
+>>>>>>> ac6278f88983 (refactor, validation: Return fatal errors from FlushStateToDisk)
     m_node.validation_signals->SyncWithValidationInterfaceQueue();
     BOOST_CHECK(!sub->m_did_flush);
 
+<<<<<<< HEAD
     SetMockTime(GetTime<std::chrono::minutes>() + DATABASE_WRITE_INTERVAL_MAX);
     chainstate.FlushStateToDisk(state_dummy, FlushStateMode::PERIODIC);
+||||||| parent of ac6278f88983 (refactor, validation: Return fatal errors from FlushStateToDisk)
+    SetMockTime(GetTime<std::chrono::minutes>() + 70min);
+    chainstate.FlushStateToDisk(state_dummy, FlushStateMode::PERIODIC);
+=======
+    SetMockTime(GetTime<std::chrono::minutes>() + 70min);
+    BOOST_CHECK(chainstate.FlushStateToDisk(state_dummy, FlushStateMode::PERIODIC));
+>>>>>>> ac6278f88983 (refactor, validation: Return fatal errors from FlushStateToDisk)
     m_node.validation_signals->SyncWithValidationInterfaceQueue();
     BOOST_CHECK(sub->m_did_flush);
 }
