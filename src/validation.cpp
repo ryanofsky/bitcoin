341e97663bc0fca8cbd8a8fe4fdf2f2cd204b6da
Ryan Ofsky <ryan@ofsky.org>
2024-03-20 17:12:09 -0400

    refactor, blockstorage: Return fatal error from LoadBlockIndex
    Return fatal error and interrupt status from LoadBlockIndex functions and
    update callers to use new result types.

--- src/validation.cpp
+++ src/validation.cpp
@@ -80,6 +80,9 @@ using kernel::CCoinsStats;
 using kernel::CoinStatsHashType;
 using kernel::ComputeUTXOStats;
 using kernel::FlushResult;
+using kernel::Interrupted;
+using kernel::InterruptResult;
+using kernel::IsInterrupted;
 using kernel::Notifications;
 
 using fsbridge::FopenFn;
@@ -4791,14 +4794,15 @@ void Chainstate::ClearBlockIndexCandidates()
     setBlockIndexCandidates.clear();
 }
 
-bool ChainstateManager::LoadBlockIndex()
+util::Result<InterruptResult, AbortFailure> ChainstateManager::LoadBlockIndex()
 {
     AssertLockHeld(cs_main);
+    util::Result<InterruptResult, AbortFailure> result;
     // Load block index from databases
     bool needs_init = fReindex;
     if (!fReindex) {
-        bool ret{m_blockman.LoadBlockIndexDB(SnapshotBlockhash())};
-        if (!ret) return false;
+        result.Update(m_blockman.LoadBlockIndexDB(SnapshotBlockhash()));
+        if (!result || IsInterrupted(*result)) return result;
 
         m_blockman.ScanAndUnlinkAlreadyPrunedFiles();
 
@@ -4807,7 +4811,10 @@ bool ChainstateManager::LoadBlockIndex()
                   CBlockIndexHeightOnlyComparator());
 
         for (CBlockIndex* pindex : vSortedByHeight) {
-            if (m_interrupt) return false;
+            if (m_interrupt) {
+                result.Update(Interrupted{});
+                return result;
+            }
             // If we have an assumeutxo-based chainstate, then the snapshot
             // block will be a candidate for the tip, but it may not be
             // VALID_TRANSACTIONS (eg if we haven't yet downloaded the block),
@@ -4840,7 +4847,7 @@ bool ChainstateManager::LoadBlockIndex()
 
         LogPrintf("Initializing databases...\n");
     }
-    return true;
+    return result;
 }
 
 bool Chainstate::LoadGenesisBlock()
