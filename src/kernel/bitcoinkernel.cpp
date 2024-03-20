fb11826285fa64f7631ba1d9e36f7c4635f4366c
Ryan Ofsky <ryan@ofsky.org>
2024-03-20 17:12:09 -0400

    refactor, validation: Return fatal errors from FlushStateToDisk
    Return fatal errors from the Chainstate::FlushStateToDisk method and several
    small, related methods which wrap it: ForceFlushStateToDisk, PruneAndFlush,
    ResizeCoinsCaches, and MaybeRebalanceCaches.
    
    Also add nodiscard annotations so callers do not accidentally ignore the result
    values. Callers in init and rpc files are updated to explicitly ignore the
    flush results, and other callers (AcceptToMemoryPool, ProcessNewPackage,
    DisconnectTip, ConnectTip, ActivateBestChainStep, ActivateSnapshot,
    MaybeCompleteSnapshotValidation) are updated to store the results in this
    commit, and will be updated in upcoming commits to bubble results up to their
    callers.

--- src/kernel/bitcoinkernel.cpp
+++ src/kernel/bitcoinkernel.cpp
@@ -15,6 +15,7 @@
 #include <kernel/checks.h>
 #include <kernel/context.h>
 #include <kernel/notifications_interface.h>
+#include <kernel/result.h>
 #include <kernel/warning.h>
 #include <logging.h>
 #include <node/blockstorage.h>
@@ -46,8 +47,8 @@
 #include <span>
 #include <stdexcept>
 #include <string>
-#include <tuple>
 #include <utility>
+#include <variant>
 #include <vector>
 
 using kernel::ChainstateRole;
@@ -1056,7 +1057,8 @@ void btck_chainstate_manager_destroy(btck_ChainstateManager* chainman)
         LOCK(btck_ChainstateManager::get(chainman).m_chainman->GetMutex());
         for (const auto& chainstate : btck_ChainstateManager::get(chainman).m_chainman->m_chainstates) {
             if (chainstate->CanFlushToDisk()) {
-                chainstate->ForceFlushStateToDisk();
+                auto flush_result{chainstate->ForceFlushStateToDisk()};
+                if (!flush_result) LogError("%s", util::ErrorString(flush_result).original);
                 chainstate->ResetCoinsViews();
             }
         }
