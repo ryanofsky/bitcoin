e8af9459cd4f90e29b4a64cdf8b646c3bbaa1be1
Ryan Ofsky <ryan@ofsky.org>
2024-05-07 08:55:34 -0400

    tools: Use ECC_Context helper in bitcoin-tx and bitcoin-wallet tools

--- src/bitcoin-wallet.cpp
+++ src/bitcoin-wallet.cpp
@@ -132,10 +132,9 @@ MAIN_FUNCTION
         return EXIT_FAILURE;
     }
 
-    ECC_Start();
+    ECC_Context ecc_context{};
     if (!wallet::WalletTool::ExecuteWalletToolFunc(args, command->command)) {
         return EXIT_FAILURE;
     }
-    ECC_Stop();
     return EXIT_SUCCESS;
 }
