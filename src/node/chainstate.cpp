c2bc6d6a367a3c38afae6ecd8477272d692af6e9
Ryan Ofsky <ryan@ofsky.org>
2024-05-30 13:26:01 -0400

    refactor: Add ChainstateManager::m_chainstates member
    Use to replace m_active_chainstate, m_ibd_chainstate, and m_snapshot_chainstate
    members. This has several benefits:
    
    - Ensures ChainstateManager treats chainstates instances equally, making
      distinctions based on their attributes, not having special cases and making
      assumptions based on their identities.
    
    - Normalizes ChainstateManager representation so states that should be
      impossible to reach and validation code has no handling for (like
      m_snapshot_chainstate being set and m_ibd_chainstate being unset, or both
      being set but m_active_chainstate pointing to the m_ibd_chainstate) can no
      longer be represented.
    
    - Makes ChainstateManager more extensible so new chainstates can be added for
      different purposes, like indexing or generating and validating assumeutxo
      snapshots without interrupting regular node operations. With the
      m_chainstates member, new chainstates can be added and handled without needing
      to make changes all over validation code or to copy/paste/modify the existing
      code that's been already been written to handle m_ibd_chainstate and
      m_snapshot_chainstate.
    
    - Avoids terms that are confusing and misleading:
    
      - The term "active chainstate" term is confusing because multiple chainstates
        will be active and in use at the same time. Before a snapshot is validated,
        wallet code will use the snapshot chainstate, while indexes will use the IBD
        chainstate, and netorking code will use both chainstates, downloading
        snapshot blocks at higher priority, but also IBD blocks simultaneously.
    
      - The term "snapshot chainstate" is ambiguous because it could refer either
        to the chainstate originally loaded from a snapshot, or to the chainstate
        being used to validate a snapshot that was loaded, or to a chainstate being
        used to produce a snapshot, but it is arbitrary used to refer the first
        thing. The terms "most-work chainstate" or "assumed-valid chainstate" should
        be less ambiguous ways to refer to chainstates loaded from snapshots.
    
      - The term "IBD chainstate" is not just ambiguous because actively confusing
        because technically IBD ends and the node is considered synced when the
        snapshot chainstate finishes syncing, so in practice the IBD chainstate
        will mostly by synced after IBD is complete. The term "fully-validated" is
        a better way of describing the characteristics and purpose of this
        chainstate.

--- src/node/chainstate.cpp
+++ src/node/chainstate.cpp
@@ -174,7 +174,7 @@ ChainstateLoadResult LoadChainstate(ChainstateManager& chainman, const CacheSize
         // Reset chainstate target to network tip instead of snapshot block.
         validated_cs.SetTargetBlock(nullptr);
         LogPrintf("[snapshot] deleting snapshot chainstate due to reindexing\n");
-        if (!chainman.DeleteSnapshotChainstate()) {
+        if (!chainman.DeleteChainstate(*assumed_valid_cs)) {
             return {ChainstateLoadStatus::FAILURE_FATAL, Untranslated("Couldn't remove snapshot chainstate.")};
         }
         assumed_valid_cs = nullptr;
