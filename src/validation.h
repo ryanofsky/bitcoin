68ffad5769f8642974cfa3c34b9c4053b9ce1e2e
Ryan Ofsky <ryan@ofsky.org>
2024-05-30 16:30:22 -0400

    refactor: Delete ChainstateManager::SnapshotBlockhash() method
    SnapshotBlockhash() is only called two places outside of tests, and is used
    redundantly in some tests, checking the same field as other checks. Simplify by
    dropping the method and using the m_from_snapshot_blockhash field directly.

--- src/validation.h
+++ src/validation.h
@@ -1183,8 +1183,6 @@ public:
      */
     mutable VersionBitsCache m_versionbitscache;
 
-    std::optional<uint256> SnapshotBlockhash() const;
-
     /** Check whether we are doing an initial block download (synchronizing from disk or network) */
     bool IsInitialBlockDownload() const;
 
