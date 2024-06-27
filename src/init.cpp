475b70da309bbfd889968ce87d251f539f1d3351
Ryan Ofsky <ryan@ofsky.org>
2024-06-27 08:22:23 -0400

    logging: Use LogFatal instead LogError for fatal errors
    Keep using LogError for nonfatal errors. Also rename LogWarning to LogCritical
    to be clear it is only intended to be used for "severe problems that the
    node admin should address," as described in the documentation.

--- src/init.cpp
+++ src/init.cpp
@@ -841,7 +841,7 @@ std::set<BlockFilterType> g_enabled_filter_types;
     // Since LogPrintf may itself allocate memory, set the handler directly
     // to terminate first.
     std::set_new_handler(std::terminate);
-    LogError("Out of memory. Terminating.\n");
+    LogFatal("Out of memory. Terminating.\n");
 
     // The log was successful, terminate now.
     std::terminate();
@@ -1177,7 +1177,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
     scheduler.scheduleEvery([&args, &node]{
         constexpr uint64_t min_disk_space = 50 << 20; // 50 MB
         if (!CheckDiskSpace(args.GetBlocksDirPath(), min_disk_space)) {
-            LogError("Shutting down due to lack of disk space!\n");
+            LogFatal("Shutting down due to lack of disk space!\n");
             if (!(*Assert(node.shutdown))()) {
                 LogError("Failed to send shutdown signal after disk space check\n");
             }
@@ -1584,7 +1584,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
             try {
                 return f();
             } catch (const std::exception& e) {
-                LogError("%s\n", e.what());
+                LogFatal("%s\n", e.what());
                 return std::make_tuple(node::ChainstateLoadStatus::FAILURE, _("Error opening block database"));
             }
         };
@@ -1592,7 +1592,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
         if (status == node::ChainstateLoadStatus::SUCCESS) {
             uiInterface.InitMessage(_("Verifying blocks…").translated);
             if (chainman.m_blockman.m_have_pruned && options.check_blocks > MIN_BLOCKS_TO_KEEP) {
-                LogWarning("pruned datadir may not have more than %d blocks; only checking available blocks\n",
+                LogCritical("pruned datadir may not have more than %d blocks; only checking available blocks\n",
                                   MIN_BLOCKS_TO_KEEP);
             }
             std::tie(status, error) = catch_exceptions([&]{ return VerifyLoadedChainstate(chainman, options);});
@@ -1619,7 +1619,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
                         LogError("Internal error: failed to reset shutdown signal.\n");
                     }
                 } else {
-                    LogError("Aborted block database rebuild. Exiting.\n");
+                    LogFatal("Aborted block database rebuild. Exiting.\n");
                     return false;
                 }
             } else {
