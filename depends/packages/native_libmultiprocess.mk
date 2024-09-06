3749b730171e93a0beb3dcc458d440ee7035b3b9
Ryan Ofsky <ryan@ofsky.org>
2024-09-06 18:23:54 -0400

    attempt to fix fatal error: ../ipc/capnp/mining.capnp.h file not found https://cirrus-ci.com/task/6743063731634176

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=a9e16da55e880e5a0aed5008307ea56edc33fbd1
+$(package)_version=66e12f1fae6458788d234e8c1a0f43d34e72640b
 $(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=588afeaa8751ef56fe5bfdf1e40587809bcb617e4d2825064b185860977a4b5f
+$(package)_sha256_hash=96a76c360485d839384a4f3f5975c981e4a72cb38b5280c377b2b4705dbc2da2
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds
