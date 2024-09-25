0e3aedf80e3fc9040b8bc1bee735e568f4638996
Ryan Ofsky <ryan@ofsky.org>
2024-09-25 14:42:12 -0400

    common: Grammar / formatting tweaks
    Implement two cleanup suggestions from l0rinc:
    
    https://github.com/bitcoin/bitcoin/pull/16545#discussion_r1773221823
    https://github.com/bitcoin/bitcoin/pull/16545#discussion_r1773291395

--- src/common/args.cpp
+++ src/common/args.cpp
@@ -101,7 +101,7 @@ KeyInfo InterpretKey(std::string key)
  * @param[in]   flags    ArgsManager registered argument flags
  * @param[out]  error    Error description if settings value is not valid
  *
- * @return parsed settings value if it is valid, otherwise nullopt accompanied
+ * @return parsed settings value if it is valid, otherwise `nullopt` accompanied
  * by a descriptive error string
  */
 std::optional<common::SettingsValue> InterpretValue(const KeyInfo& key, const std::string* value,
@@ -121,7 +121,7 @@ std::optional<common::SettingsValue> InterpretValue(const KeyInfo& key, const st
         return false;
     }
     if (!value && (flags & ArgsManager::DISALLOW_ELISION)) {
-        error = strprintf("Can not set -%s with no value. Please specify value with -%s=value.", key.name, key.name);
+        error = strprintf("Cannot set -%s with no value. Please specify value with -%s=value.", key.name, key.name);
         return std::nullopt;
     }
     return value ? *value : "";
