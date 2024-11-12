ad32a27e966eac118587cf0e03c1eedaf0c051ed
Ryan Ofsky <ryan@ofsky.org>
2024-11-12 13:45:58 -0500

    scripted-diff: Replace AddArgs / GetArgs calls with Setting Register / Get calls
    -BEGIN VERIFY SCRIPT-
    python contrib/devtools/reg-settings.py
    git add -N src/bench/bench_bitcoin_settings.h src/bitcoin-tx_settings.h src/bitcoin-util_settings.h src/bitcoin-wallet_settings.h src/chainparamsbase_settings.h src/common/args_settings.h src/init/common_settings.h src/init_settings.h src/qt/bitcoin_settings.h src/test/argsman_tests_settings.h src/test/logging_tests_settings.h src/wallet/init_settings.h
    git rm contrib/devtools/reg-settings.py
    -END VERIFY SCRIPT-

--- src/bench/bench_bitcoin_settings.h
+++ src/bench/bench_bitcoin_settings.h
@@ -5,9 +5,50 @@
 #ifndef BENCH_BENCH_BITCOIN_SETTINGS_H
 #define BENCH_BENCH_BITCOIN_SETTINGS_H
 
+#include <common/setting.h>
+
+#include <string>
+#include <vector>
+
 static const char* DEFAULT_BENCH_FILTER = ".*";
 static constexpr int64_t DEFAULT_MIN_TIME_MS{10};
 /** Priority level default value, run "all" priority levels */
 static const std::string DEFAULT_PRIORITY{"all"};
+
+using AsymptoteSetting = common::Setting<
+    "-asymptote=<n1,n2,n3,...>", std::string, {.legacy = true},
+    "Test asymptotic growth of the runtime of an algorithm, if supported by the benchmark">;
+
+using FilterSetting = common::Setting<
+    "-filter=<regex>", std::string, {.legacy = true},
+    "Regular expression filter to select benchmark by name (default: %s)">
+    ::DefaultFn<[] { return DEFAULT_BENCH_FILTER; }>;
+
+using ListSetting = common::Setting<
+    "-list", bool, {.legacy = true},
+    "List benchmarks without executing them">;
+
+using MinTimeSetting = common::Setting<
+    "-min-time=<milliseconds>", int64_t, {.legacy = true, .disallow_negation = true},
+    "Minimum runtime per benchmark, in milliseconds (default: %d)">
+    ::Default<DEFAULT_MIN_TIME_MS>;
+
+using OutputCsvSetting = common::Setting<
+    "-output-csv=<output.csv>", fs::path, {.legacy = true},
+    "Generate CSV file with the most important benchmark results">;
+
+using OutputJsonSetting = common::Setting<
+    "-output-json=<output.json>", fs::path, {.legacy = true},
+    "Generate JSON file with all benchmark results">;
+
+using SanityCheckSetting = common::Setting<
+    "-sanity-check", bool, {.legacy = true},
+    "Run benchmarks for only one iteration with no output">;
+
+using PriorityLevelSetting = common::Setting<
+    "-priority-level=<l1,l2,l3>", std::string, {.legacy = true},
+    "Run benchmarks of one or multiple priority level(s) (%s), default: '%s'">
+    ::DefaultFn<[] { return DEFAULT_PRIORITY; }>
+    ::HelpFn<[](const auto& fmt) { return strprintf(fmt, benchmark::ListPriorities(), DEFAULT_PRIORITY); }>;
 
 #endif // BENCH_BENCH_BITCOIN_SETTINGS_H
