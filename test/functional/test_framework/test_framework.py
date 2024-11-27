5e17c2ee279abe44fe4b1c020b40ed4482877c61
Ryan Ofsky <ryan@ofsky.org>
2024-11-26 22:53:46 -0500

    test: add interface_ipc_mining.py test calling bitcoin-mine
    Co-authored-by: Sjors Provoost <sjors@sprovoost.nl>

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -290,22 +290,57 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
 
         paths = types.SimpleNamespace()
         binaries = {
+<<<<<<< HEAD
             "bitcoind": "BITCOIND",
             "bitcoin-cli": "BITCOINCLI",
             "bitcoin-util": "BITCOINUTIL",
             "bitcoin-tx": "BITCOINTX",
             "bitcoin-chainstate": "BITCOINCHAINSTATE",
             "bitcoin-wallet": "BITCOINWALLET",
+||||||| parent of 27c6576aba5c (test: add interface_ipc_mining.py test calling bitcoin-mine)
+            "bitcoind": ("bitcoind", "BITCOIND"),
+            "bitcoin-cli": ("bitcoincli", "BITCOINCLI"),
+            "bitcoin-util": ("bitcoinutil", "BITCOINUTIL"),
+            "bitcoin-chainstate": ("bitcoinchainstate", "BITCOINCHAINSTATE"),
+            "bitcoin-wallet": ("bitcoinwallet", "BITCOINWALLET"),
+=======
+            "bitcoind": ("bitcoind", "BITCOIND"),
+            "bitcoin-cli": ("bitcoincli", "BITCOINCLI"),
+            "bitcoin-util": ("bitcoinutil", "BITCOINUTIL"),
+            "bitcoin-chainstate": ("bitcoinchainstate", "BITCOINCHAINSTATE"),
+            "bitcoin-mine": ("bitcoin_mine", "BITCOINMINE"),
+            "bitcoin-wallet": ("bitcoinwallet", "BITCOINWALLET"),
+>>>>>>> 27c6576aba5c (test: add interface_ipc_mining.py test calling bitcoin-mine)
         }
+<<<<<<< HEAD
         for binary, env_variable_name in binaries.items():
             default_filename = os.path.join(
+||||||| parent of 27c6576aba5c (test: add interface_ipc_mining.py test calling bitcoin-mine)
+        for binary, [attribute_name, env_variable_name] in binaries.items():
+            default_filename = os.path.join(
+=======
+        def binary_path(binary):
+            return os.path.join(
+>>>>>>> 27c6576aba5c (test: add interface_ipc_mining.py test calling bitcoin-mine)
                 self.config["environment"]["BUILDDIR"],
                 "bin",
                 binary + self.config["environment"]["EXEEXT"],
             )
+<<<<<<< HEAD
             setattr(paths, env_variable_name.lower(), os.getenv(env_variable_name, default=default_filename))
+||||||| parent of 27c6576aba5c (test: add interface_ipc_mining.py test calling bitcoin-mine)
+            setattr(paths, attribute_name, os.getenv(env_variable_name, default=default_filename))
+=======
+
+        # Set paths to bitcoin binaries allowing overrides with environment
+        # variables.
+        for binary, [attribute_name, env_variable_name] in binaries.items():
+            setattr(paths, attribute_name, os.getenv(env_variable_name) or binary_path(binary))
+
+>>>>>>> 27c6576aba5c (test: add interface_ipc_mining.py test calling bitcoin-mine)
         # BITCOIN_CMD environment variable can be specified to invoke bitcoin
         # wrapper binary instead of other executables.
+        paths.bitcoin_bin = binary_path("bitcoin")
         paths.bitcoin_cmd = shlex.split(os.getenv("BITCOIN_CMD", "")) or None
         return paths
 
@@ -1023,6 +1058,11 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
         if not self.is_cli_compiled():
             raise SkipTest("bitcoin-cli has not been compiled.")
 
+    def skip_if_no_ipc(self):
+        """Skip the running test if ipc is not enabled."""
+        if not self.is_ipc_enabled():
+            raise SkipTest("ipc is not enabled.")
+
     def skip_if_no_previous_releases(self):
         """Skip the running test if previous releases are not available."""
         if not self.has_previous_releases():
@@ -1081,6 +1121,10 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
     def is_usdt_compiled(self):
         """Checks whether the USDT tracepoints were compiled."""
         return self.config["components"].getboolean("ENABLE_USDT_TRACEPOINTS")
+
+    def is_ipc_enabled(self):
+        """Checks whether ipc is enabled."""
+        return self.config["components"].getboolean("ENABLE_IPC")
 
     def has_blockfile(self, node, filenum: str):
         return (node.blocks_path/ f"blk{filenum}.dat").is_file()
