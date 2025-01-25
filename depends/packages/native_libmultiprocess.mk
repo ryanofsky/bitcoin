b62cf45ce0258faf0ef4dbe1a1f8c48e8d15ee18
Ryan Ofsky <ryan@ofsky.org>
2025-01-25 13:33:44 -0500

    depends: Switch libmultiprocess packages to use local git subtree
    With newly introduced libmultiprocess subtree, there's no need for depends
    system to download and track changes to the upstream repository.
    
    Note that adding the libmultiprocess subtree does not allow dropping
    libmultiprocess packages from the depends build, because libmultiprocess
    includes a code generation tool called mpgen, and in cross-compiled builds,
    bitcoin core's cmake build system doesn't have access to a native toolchain and
    can't build mpgen itself, so the depends system (or the native environment if
    not using depends) needs to supply it.

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,17 @@
 package=native_libmultiprocess
+<<<<<<< HEAD
 $(package)_version=9ba88dc2c7f4df1356c67e58e709f1609d336689
 $(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
 $(package)_sha256_hash=515adff60182aef9aa26bbeee32519ca2b3ed5926db91a9236e913c61876f6db
+||||||| parent of 97058b53e843 (depends: Switch libmultiprocess packages to use local git subtree)
+$(package)_version=9558ceb0d47ac1f62f88d29ec55f8f3099e32b20
+$(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
+$(package)_file_name=$($(package)_version).tar.gz
+$(package)_sha256_hash=9dae2d4c352fc9a12968b0a05b11faac07900135a5dc8a335e1195d00908314a
+=======
+$(package)_local_dir=../src/ipc/libmultiprocess
+>>>>>>> 97058b53e843 (depends: Switch libmultiprocess packages to use local git subtree)
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds
