598915101ddedb0b2cf35d42fd466e30a9bfdbe1
Ryan Ofsky <ryan@ofsky.org>
2025-04-02 17:40:52 -0400

    wallet, logging: Replace WalletLogPrintf() with LogInfo()
    Make new logging macros LogDebug(), LogTrace(), LogInfo(), LogWarning(), and
    LogError() compatible with wallet code and drop custom WalletLogPrintf()
    function. The new logging macros introduced in #28318 weren't previously useful
    in wallet code because wallet code prefixes most log messages with the wallet
    names to be be able to distinguish log output from multiple wallets. Fix this
    by introducing a new WalletLogContext class inheriting from BCLog::Context
    which can include the wallet name in log messages

--- src/wallet/scriptpubkeyman.cpp
+++ src/wallet/scriptpubkeyman.cpp
@@ -20,6 +20,7 @@
 #include <util/string.h>
 #include <util/time.h>
 #include <util/translation.h>
+#include <wallet/logging.h>
 
 #include <optional>
 
@@ -293,7 +294,7 @@ bool LegacyDataSPKM::LoadCScript(const CScript& redeemScript)
     if (redeemScript.size() > MAX_SCRIPT_ELEMENT_SIZE)
     {
         std::string strAddr = EncodeDestination(ScriptHash(redeemScript));
-        WalletLogPrintf("%s: Warning: This wallet contains a redeemScript of size %i which exceeds maximum size %i thus can never be redeemed. Do not use address %s.\n", __func__, redeemScript.size(), MAX_SCRIPT_ELEMENT_SIZE, strAddr);
+        LogInfo(m_log, "%s: Warning: This wallet contains a redeemScript of size %i which exceeds maximum size %i thus can never be redeemed. Do not use address %s.\n", __func__, redeemScript.size(), MAX_SCRIPT_ELEMENT_SIZE, strAddr);
         return true;
     }
 
@@ -1151,8 +1152,16 @@ std::vector<WalletDestination> DescriptorScriptPubKeyMan::MarkUnusedAddresses(co
     std::vector<WalletDestination> result;
     if (IsMine(script)) {
         int32_t index = m_map_script_pub_keys[script];
+<<<<<<< HEAD
         if (index >= m_wallet_descriptor.GetNext()) {
             WalletLogPrintf("%s: Detected a used keypool item at index %d, mark all keypool items up to this item as used\n", __func__, index);
+||||||| parent of 23013d534b6 (wallet, logging: Replace WalletLogPrintf() with LogInfo())
+        if (index >= m_wallet_descriptor.next_index) {
+            WalletLogPrintf("%s: Detected a used keypool item at index %d, mark all keypool items up to this item as used\n", __func__, index);
+=======
+        if (index >= m_wallet_descriptor.next_index) {
+            LogInfo(m_log, "%s: Detected a used keypool item at index %d, mark all keypool items up to this item as used\n", __func__, index);
+>>>>>>> 23013d534b6 (wallet, logging: Replace WalletLogPrintf() with LogInfo())
             auto out_keys = std::make_unique<FlatSigningProvider>();
             std::vector<CScript> scripts_temp;
             while (index >= m_wallet_descriptor.GetNext()) {
@@ -1166,7 +1175,7 @@ std::vector<WalletDestination> DescriptorScriptPubKeyMan::MarkUnusedAddresses(co
             }
         }
         if (!TopUp()) {
-            WalletLogPrintf("%s: Topping up keypool failed (locked wallet)\n", __func__);
+            LogInfo(m_log, "%s: Topping up keypool failed (locked wallet)\n", __func__);
         }
     }
 
