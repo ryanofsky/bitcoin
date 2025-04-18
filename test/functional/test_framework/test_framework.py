a4e5d44d420afd8368b2de2b30d92228a85c0d93
Ryan Ofsky <ryan@ofsky.org>
2025-04-18 11:37:21 -0400

    test: add interface_ipc_cli.py testing bitcoin-cli -ipcconnect
    Co-authored-by: Sjors Provoost <sjors@sprovoost.nl>

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -291,16 +291,24 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
             "bitcoin-chainstate": ("bitcoinchainstate", "BITCOINCHAINSTATE"),
             "bitcoin-wallet": ("bitcoinwallet", "BITCOINWALLET"),
         }
-        for binary, [attribute_name, env_variable_name] in binaries.items():
-            default_filename = os.path.join(
+        def binary_path(binary):
+            return os.path.join(
                 self.config["environment"]["BUILDDIR"],
                 "bin",
                 binary + self.config["environment"]["EXEEXT"],
             )
+<<<<<<< HEAD
             setattr(paths, attribute_name, os.getenv(env_variable_name, default=default_filename))
         # BITCOIN_CMD environment variable can be specified to invoke bitcoin
         # wrapper binary instead of other executables.
         paths.bitcoin_cmd = shlex.split(os.getenv("BITCOIN_CMD", "")) or None
+||||||| parent of 6bf727fb8800 (test: add interface_ipc_cli.py testing bitcoin-cli -ipcconnect)
+            setattr(paths, attribute_name, os.getenv(env_variable_name, default=default_filename))
+=======
+        for binary, [attribute_name, env_variable_name] in binaries.items():
+            setattr(paths, attribute_name, os.getenv(env_variable_name) or binary_path(binary))
+        paths.bitcoin_node = binary_path("bitcoin-node")
+>>>>>>> 6bf727fb8800 (test: add interface_ipc_cli.py testing bitcoin-cli -ipcconnect)
         return paths
 
     def get_binaries(self, bin_dir=None):
@@ -1007,6 +1015,11 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
         if not self.is_cli_compiled():
             raise SkipTest("bitcoin-cli has not been compiled.")
 
+    def skip_if_no_ipc(self):
+        """Skip the running test if ipc is not enabled."""
+        if not self.is_ipc_enabled():
+            raise SkipTest("ipc is not enabled.")
+
     def skip_if_no_previous_releases(self):
         """Skip the running test if previous releases are not available."""
         if not self.has_previous_releases():
@@ -1061,6 +1074,10 @@ class BitcoinTestFramework(metaclass=BitcoinTestMetaClass):
     def is_usdt_compiled(self):
         """Checks whether the USDT tracepoints were compiled."""
         return self.config["components"].getboolean("ENABLE_USDT_TRACEPOINTS")
+
+    def is_ipc_enabled(self):
+        """Checks whether ipc is enabled."""
+        return self.config["components"].getboolean("ENABLE_IPC")
 
     def has_blockfile(self, node, filenum: str):
         return (node.blocks_path/ f"blk{filenum}.dat").is_file()
