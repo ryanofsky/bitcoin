0ca362a7c71f73243c894f7edf2cc8e8bd25d5fb
Ryan Ofsky <ryan@ofsky.org>
2025-04-30 08:39:29 -0400

    ipc, refactor: Add Stream type alias and use it
    Use Stream type to abstract socket ids and be compatible with updated
    mp::ConnectStream() and mp::ServeStream() functions that use streams
    instead of socket ids in an upcoming version of libmultiprocess which
    adds windows support.
    
    Since creating Stream objects from socket ids can require the event loop
    to be running, the ipc::Protocol::serve() method is also updated to
    accept the server stream though a callback parameter instead of a normal
    parameter.

--- src/ipc/util.h
+++ src/ipc/util.h
@@ -8,19 +8,36 @@
 #include <tinyformat.h>
 #include <util/strencodings.h>
 
+#include <array>
 #include <cstdint>
 #include <functional>
+#include <kj/debug.h>
 #include <mp/util.h>
 #include <mp/version.h>
+#include <sys/socket.h>
 
 namespace mp {
 // Definitions that can be deleted when libmultiprocess subtree is updated to
 // v14. Having these allows Bitcoin Core changes to be decoupled from
 // libmultiprocess changes so they don't have to be reviewed in a single PR.
 #if MP_MAJOR_VERSION < 14
+class EventLoop;
 using ProcessId = int;
 using SocketId = int;
 constexpr SocketId SocketError{-1};
+
+inline std::array<SocketId, 2> SocketPair()
+{
+    int pair[2];
+    KJ_SYSCALL(socketpair(AF_UNIX, SOCK_STREAM, 0, pair));
+    return {pair[0], pair[1]};
+}
+
+using Stream = SocketId;
+inline Stream MakeStream(EventLoop&, SocketId socket)
+{
+    return socket;
+}
 
 using SpawnConnectInfo = std::string;
 inline SocketId StartSpawned(const SpawnConnectInfo& connect_info)
