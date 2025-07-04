6e3bfb2b6df4774c1009d9210bc38f6db1a83c6d
Ryan Ofsky <ryan@ofsky.org>
2025-07-04 11:07:38 -0400

    Build internal binaries in libexec/ instead of bin/
    Add symlinks for convenience & compatibility.
    
    Discussed in https://github.com/bitcoin/bitcoin/pull/31679#discussion_r2184924462

--- cmake/module/InstallBinaryComponent.cmake
+++ cmake/module/InstallBinaryComponent.cmake
@@ -14,7 +14,17 @@ function(install_binary_component component)
   )
   set(target_name ${component})
   if(IC_INTERNAL)
+    # Install in libexec/ instead of bin/
     set(runtime_dest ${CMAKE_INSTALL_LIBEXECDIR})
+    # Build in libexec/ instead of bin/ so build layout matches install layout.
+    set_target_properties(${target_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/${runtime_dest}")
+    # Create symlink from build/bin/ to build/libexec/ for convenience.
+    set(filename "${CMAKE_EXECUTABLE_PREFIX}${target_name}${CMAKE_EXECUTABLE_SUFFIX}")
+    add_custom_target(${target_name}_bin_symlink
+        COMMAND ${CMAKE_COMMAND} -E create_symlink "${PROJECT_BINARY_DIR}/${runtime_dest}/${filename}" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${filename}"
+        VERBATIM
+    )
+    add_dependencies(${target_name} ${target_name}_bin_symlink)
   else()
     set(runtime_dest ${CMAKE_INSTALL_BINDIR})
   endif()
