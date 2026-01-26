698a05427ed3bdd8255840ce963d7c8d6e7c3e92
stickies-v <stickies-v@protonmail.com>
2026-01-26 18:48:08 +0000

    logging: use util/log.h where possible
    Preparation for a future commit where kernel's dependency
    on logging.cpp is removed completely.
    
    Replace usage of logging\.h with util/log\.h where it
    suffices, and fix wrong includes according to iwyu.

--- src/util/batchpriority.cpp
+++ src/util/batchpriority.cpp
@@ -2,6 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <util/batchpriority.h>
+
 #include <logging.h>
 #include <util/syserror.h>
 
