a4fc40e03b151ba18b4e4b9cbfbf54b418d35933
Ryan Ofsky <ryan@ofsky.org>
2026-04-17 11:17:39 -0400

    util: drop POSIX/pthread dependencies to enable MSVC builds
    Remove POSIX and pthread calls from util.cpp to avoid relying on MinGW's POSIX
    compatibility layer. This lets code be compiled with MSVC.

--- src/ipc/libmultiprocess/src/mp/util.cpp
+++ src/ipc/libmultiprocess/src/mp/util.cpp
@@ -12,7 +12,6 @@
 #include <kj/common.h>
 #include <kj/debug.h>
 #include <kj/string-tree.h>
-#include <pthread.h>
 #include <sstream>
 #include <string>
 #include <system_error>
@@ -26,11 +25,13 @@
 #include <winsock2.h>
 #else
 #include <fcntl.h>
+#include <pthread.h>
 #include <sys/resource.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#define _getpid getpid
 #endif
 
 #ifdef __linux__
@@ -80,12 +81,12 @@ size_t MaxFd()
 std::string ThreadName(const char* exe_name)
 {
     char thread_name[16] = {0};
-#ifdef HAVE_PTHREAD_GETNAME_NP
+#if defined(HAVE_PTHREAD_GETNAME_NP) && !defined(WIN32)
     pthread_getname_np(pthread_self(), thread_name, sizeof(thread_name));
 #endif // HAVE_PTHREAD_GETNAME_NP
 
     std::ostringstream buffer;
-    buffer << (exe_name ? exe_name : "") << "-" << getpid() << "/";
+    buffer << (exe_name ? exe_name : "") << "-" << _getpid() << "/";
 
     if (thread_name[0] != '\0') {
         buffer << thread_name << "-";
